0a974e7ac829f3cd94f2437dca1912f6799562db
roma <v3designer@outlook.com>
2019-12-06 14:16:24 +0300

    Небольшие измения в формате карты, ходьба на сломанных ногах

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from math import atan, degrees, cos, sin, radians, sqrt
-from random import randint
+from random import randint, choice
 
 
 class Object:
@@ -13,6 +13,7 @@ class Object:
     def rotate(self, angle):
         self.transformed_texture = pygame.transform.rotate(self.orig_texture, 360 - self.rotation)
         self.rotation = angle
+        self.geometryToOrigin()
 
     def geometryToOrigin(self):
         bound_box = self.transformed_texture.get_bounding_rect()  # достаём рамку текстуры
@@ -59,10 +60,11 @@ class Weapon(Object):
         # ------крутим тело------
         x = abs(self.position[0] - point[0])
         y = abs(self.position[1] - point[1])
-        print(x, y)
+        print(x, y, end="")
         r = sqrt(x ** 2 + y ** 2)
-        print(r)
+        print(r, end="")
         self.position = (cos(radians(self.rotation)) * r, sin(radians(self.rotation)) * r)
+        print(self.position)
         # ------крутим точку спавна пуль------
         x = abs(self.barrel_position[0] - point[0])
         y = abs(self.barrel_position[1] - point[1])
@@ -84,12 +86,13 @@ class Weapon(Object):
 
 
 class Player(Object):
-    def __init__(self, angle=0, position=(400, 300)):
+    def __init__(self, position, angle=0):
         texture = pygame.Surface((40, 40), pygame.SRCALPHA)
         texture.fill((255, 255, 255))
         super().__init__(position, angle, texture)
+        self.speed = 100
         self.viewport_position = position
-        self.position = self.viewport_position  # TODO координаты для карты
+        self.position = self.viewport_position
         self.geometryToOrigin()
         self.weapon = Weapon(0, self.rotation,
                              (self.position[0] + 20, self.position[1] - 30), 5, 30, 500, 100)
@@ -97,6 +100,11 @@ class Player(Object):
     def rotate(self, angle):
         super().rotate(angle)
         self.weapon.syncRotation(self.position, angle)
+
+    def move(self, vec2, seconds):
+        x = self.position[0] + self.speed * seconds * vec2[0]
+        y = self.position[0] + self.speed * seconds * vec2[0]
+        self.position = (self.position[0] + x, self.position[1] + y)
 
     def trackTo(self, point):
         x = point[0] - self.viewport_position[0]  # разница между точками по x
@@ -126,49 +134,77 @@ class Player(Object):
 
 
 class Map:
-    def __init__(self, map_size, *objects):
-        self.orig_canvas = pygame.Surface(map_size)
-        self.orig_canvas.fill(pygame.Color('black'))
+    def __init__(self, *objects):
+        self.map_size = (1000, 1000)
+        self.gg_spawns = [(100, 100), (400, 400)]
+        self.orig_canvas = pygame.Surface(self.map_size)
+        self.orig_canvas.fill((50, 50, 50))
         self.render_canvas = self.orig_canvas
         self.bullets = list()
+        self.player = Player(choice(self.gg_spawns))
 
     def checkCollision(self):
         pass
 
     def render(self, seconds):
-        self.render_canvas = pygame.Surface((1000, 1000))
+        self.render_canvas = pygame.Surface(self.map_size)
+
         for i in range(len(self.bullets)):
             self.bullets[i].update(seconds)
             self.render_canvas.blit(self.bullets[i].transformed_texture, self.bullets[i].position)
+
+        self.render_canvas.blit(world.player.transformed_texture, world.player.position)
+        self.render_canvas.blit(world.player.weapon.transformed_texture, world.player.weapon.position)
 
     def killBulletsInOffset(self):
         pass
 
 
-player = Player()
 world = Map((1000, 1000))
+
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('black'))
+
 fps = 60
 frame_time = 1 / fps
+
 running = True
 clock = pygame.time.Clock()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEMOTION:
-            player.trackTo(event.pos)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            world.bullets.append(player.shoot())
+            world.player.trackTo(event.pos)
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            world.bullets.append(world.player.shoot())
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        world.player.move((0, -1), frame_time)
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        world.player.move((0, 1), frame_time)
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        world.player.move((1, 0), frame_time)
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        world.player.move((-1, 0), frame_time)
+
+    """elif keys[pygame.K_UP] or keys[pygame.K_w]:
+            world.player.move((0, -1), frame_time)
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            world.player.move((0, 1), frame_time)
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            world.player.move((1, 0), frame_time)
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            world.player.move((-1, 0), frame_time)"""
 
     screen.fill(pygame.Color('black'))
     world.render(frame_time)
     screen.blit(world.render_canvas, (0, 0))
 
-    screen.blit(player.transformed_texture, player.viewport_position)
-    screen.blit(player.weapon.transformed_texture, player.weapon.position)
     pygame.display.flip()
     clock.tick(fps)
+
 pygame.quit()
